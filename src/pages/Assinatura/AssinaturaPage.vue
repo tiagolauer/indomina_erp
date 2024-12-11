0f81edd9b907a4f44cd1841915057065de53f893
Tiago Lauer <tiagoestrelalauer@gmail.com>
2024-12-11 01:30:54 -0300

    Teste

--- src/pages/Assinatura/AssinaturaPage.vue
+++ src/pages/Assinatura/AssinaturaPage.vue
@@ -9,20 +9,20 @@ import { useLoginStore } from "@/stores/login/loginStore";
 import { createCheckoutSession, createPortalCaptive } from "@/services/stripe/StripeService";
 import { ScToastUtil } from "@/utils/scToastUtil";
 
-const parametros = systemParamsAccount;
+const parametros = systemParamsAccount || {
+    quota: {
+        free: { price: 0, users: 0, services: 0, products: 0, signatures: 0, sales: 0 },
+        pro: { price: 0, users: 0, services: 0, products: 0, signatures: 0, sales: 0 },
+    },
+};
+
 const loginStore = useLoginStore();
 const proAccount = computed(() => loginStore.isProAccount);
-// Computed properties
 const isPastDue = computed(() => loginStore.statusAccount === "vencida");
-
-const hasStripeAccount = computed(() => {
-    if (loginStore.statusAccount === "inativa" || !loginStore.dataAccountLogged?.stripeCustomerId) {
-        return null;
-    }
-    return loginStore.dataAccountLogged?.stripeCustomerId;
-});
-
+const hasStripeAccount = computed(() => loginStore.statusAccount !== "inativa" && loginStore.dataAccountLogged?.stripeCustomerId);
 const showCardFree = computed(() => !proAccount.value && !isPastDue.value);
+
+let isRequestPending = false;
 
 const accessPortalCaptive = async () => {
     if (!hasStripeAccount.value) {
@@ -33,44 +33,58 @@ const accessPortalCaptive = async () => {
         const data = await createPortalCaptive(hasStripeAccount.value);
         window.open(data?.data?.url, "_self");
     } catch (error) {
+        console.error("Erro no acesso ao portal:", error);
         ScToastUtil.error("Erro ao acessar o portal, tente novamente mais tarde!");
     }
 };
 
-const sinatatureSubscription = async () => {
+const signatureSubscription = async () => {
+    if (isRequestPending) return;
+    isRequestPending = true;
+
     if (!hasStripeAccount.value) {
         ScToastUtil.warning("Por favor, recarregue a página e tente novamente mais tarde.");
+        isRequestPending = false;
         return;
     }
+
     try {
         const url = await createCheckoutSession(hasStripeAccount.value);
         window.open(url?.data, "_self");
     } catch (error) {
+        console.error("Erro no checkout:", error);
         ScToastUtil.error("Erro ao criar sessão de checkout, tente novamente mais tarde.");
+    } finally {
+        isRequestPending = false;
     }
 };
-
 </script>
 
 <template>
     <div class="container mx-auto max-w-4xl px-4 py-8">
         <div v-if="proAccount" class="mb-8 flex flex-col items-center justify-center text-center">
-            <h1 class="text-3xl font-bold text-center">Seu plano atual é PRO!</h1>
-            <p class="mt-2 text-lg max-w-md text-muted-foreground">O plano PRO é o plano ideal para o seu negócio,
-                tenha todos os recursos ilimitados e suporte total.</p>
+            <h1 class="text-3xl font-bold text-center">Seu plano atual é PRO!</h1>
+            <p class="mt-2 text-lg max-w-md text-muted-foreground">
+                O plano PRO é o plano ideal para o seu negócio, tenha todos os recursos ilimitados e suporte total.
+            </p>
         </div>
+
         <div v-else-if="isPastDue" class="mb-8 flex flex-col items-center justify-center text-center">
             <h1 class="text-3xl font-bold text-center">Sua assinatura PRO expirou!</h1>
-            <p class="mt-2 text-lg max-w-md text-muted-foreground">Com isso, seu plano fica limitado aos recursos do
-                plano Standard, acesse o link abaixo e gerencie sua assinatura para voltar a utilizar todos os recursos
-                do
-                PRO.</p>
+            <p class="mt-2 text-lg max-w-md text-muted-foreground">
+                Com isso, seu plano fica limitado aos recursos do plano Standard. Acesse o link abaixo e gerencie sua
+                assinatura
+                para voltar a utilizar todos os recursos do PRO.
+            </p>
         </div>
+
         <div v-else class="mb-8 flex flex-col items-center justify-center text-center">
             <h1 class="text-3xl font-bold text-center">Escolha o plano!</h1>
-            <p class="mt-2 text-lg max-w-md text-muted-foreground">Tenha todos os recursos ilimitados e suporte total
-                assinando o plano PRO.</p>
+            <p class="mt-2 text-lg max-w-md text-muted-foreground">
+                Tenha todos os recursos ilimitados e suporte total assinando o plano PRO.
+            </p>
         </div>
+
         <div class="flex flex-col md:flex-row justify-center gap-4">
             <Card class="md:min-w-[400px]" v-if="showCardFree">
                 <CardHeader>
@@ -78,9 +92,12 @@ const sinatatureSubscription = async () => {
                         class="flex items-center justify-between text-2xl font-bold dark:text-gray-300 text-gray-500">
                         <div>
                             <p class="text-xl font-bold">Plano Standard</p>
-                            <p class="text-3xl font-bold">{{ parametros.quota.free.price.toLocaleString('pt-BR', {
-                                style: 'currency', currency: 'BRL'
-                            }) }}</p>
+                            <p class="text-3xl font-bold">
+                                {{ parametros.quota.free.price.toLocaleString('pt-BR', {
+                                    style: 'currency', currency:
+                                        'BRL'
+                                }) }}
+                            </p>
                         </div>
                         <Badge variant="default" v-if="showCardFree">Atual</Badge>
                     </CardTitle>
@@ -124,15 +141,18 @@ const sinatatureSubscription = async () => {
                     <Button class="w-full" variant="outline">Seu plano atual</Button>
                 </CardFooter>
             </Card>
+
             <Card class="md:min-w-[400px]">
                 <CardHeader>
                     <CardTitle class="flex items-center justify-between text-2xl font-bold">
                         <div>
                             <p class="text-xl font-bold">Plano Pro</p>
-                            <p class="text-3xl font-bold dark:text-blue-500 text-blue-600">{{
-                                parametros.quota.pro.price.toLocaleString('pt-BR', {
-                                    style: 'currency', currency: 'BRL'
-                                }) }}</p>
+                            <p class="text-3xl font-bold dark:text-blue-500 text-blue-600">
+                                {{ parametros.quota.pro.price.toLocaleString('pt-BR', {
+                                    style: 'currency', currency:
+                                        'BRL'
+                                }) }}
+                            </p>
                         </div>
                         <Badge variant="default" v-if="proAccount">Atual</Badge>
                     </CardTitle>
@@ -141,27 +161,27 @@ const sinatatureSubscription = async () => {
                 <CardContent class="space-y-2">
                     <div class="flex items-center">
                         <Check class="mr-2 h-4 w-4 text-green-500" />
-                        <span>Até {{ parametros.quota.pro.users == 0 ? '♾️' : parametros.quota.pro.users }}
+                        <span>Até {{ parametros.quota.pro.users === 0 ? '♾️' : parametros.quota.pro.users }}
                             usuários</span>
                     </div>
                     <div class="flex items-center">
                         <Check class="mr-2 h-4 w-4 text-green-500" />
-                        <span>Até {{ parametros.quota.pro.services == 0 ? '♾️' : parametros.quota.pro.services }}
+                        <span>Até {{ parametros.quota.pro.services === 0 ? '♾️' : parametros.quota.pro.services }}
                             serviços</span>
                     </div>
                     <div class="flex items-center">
                         <Check class="mr-2 h-4 w-4 text-green-500" />
-                        <span>Até {{ parametros.quota.pro.products == 0 ? '♾️' : parametros.quota.pro.products }}
+                        <span>Até {{ parametros.quota.pro.products === 0 ? '♾️' : parametros.quota.pro.products }}
                             produtos</span>
                     </div>
                     <div class="flex items-center">
                         <Check class="mr-2 h-4 w-4 text-green-500" />
-                        <span>Até {{ parametros.quota.pro.signatures == 0 ? '♾️' : parametros.quota.pro.signatures }}
+                        <span>Até {{ parametros.quota.pro.signatures === 0 ? '♾️' : parametros.quota.pro.signatures }}
                             assinaturas</span>
                     </div>
                     <div class="flex items-center">
                         <Check class="mr-2 h-4 w-4 text-green-500" />
-                        <span>Até {{ parametros.quota.pro.sales == 0 ? '♾️' : parametros.quota.pro.sales }}
+                        <span>Até {{ parametros.quota.pro.sales === 0 ? '♾️' : parametros.quota.pro.sales }}
                             vendas</span>
                     </div>
                     <div class="flex items-center">
@@ -179,10 +199,9 @@ const sinatatureSubscription = async () => {
                     </div>
                 </CardContent>
                 <CardFooter>
-                    <Button v-if="showCardFree" @click="sinatatureSubscription" class="w-full">Fazer upgrade para Pro
-                        ✨</Button>
-                    <Button v-else="isProAccount" @click="accessPortalCaptive" class="w-full">Gerencie sua assinatura
-                        ✨</Button>
+                    <Button @click="proAccount ? accessPortalCaptive() : signatureSubscription()" class="w-full">
+                        {{ proAccount ? "Gerencie sua assinatura ✨" : "Fazer upgrade para Pro ✨" }}
+                    </Button>
                 </CardFooter>
             </Card>
         </div>
